2742d5ea5d98dd16fe7dca8751f279d4afaa2806
aowkoakwokwa <akwokawok2123@gmail.com>
2025-06-05 16:41:00 +0800

    update package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,12 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+export default [
   {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    rules: {}, // Tidak ada rule yang aktif
   },
 ];
-
-export default eslintConfig;
