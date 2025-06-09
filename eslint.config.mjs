a524d1f79e6e46f16edb322035550aeb38079064
aowkoakwokwa <akwokawok2123@gmail.com>
2025-06-09 08:13:40 +0800

    fix bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,27 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default [
+export default tseslint.config(
   {
-    files: ['**/*.{js,ts,jsx,tsx}'],
-    rules: {}, // Tidak ada rule yang aktif
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {},
   },
-];
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+    },
+  },
+);
